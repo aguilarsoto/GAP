e81ebc58d072a9e8955f0cf96ac0adfd9f528d6c
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2010-07-23 15:32:00 -0600

    the return value should be a string and its not suppouse to print the string from in the method

--- MissingLetters.rb
+++ MissingLetters.rb
@@ -1,12 +1,14 @@
 class MissingLetters
 
 	def getMissingLetters(line)
+		return_value = ""
 		abc = Hash[*line.each_char.collect {|key| [key, 1]}.flatten]
-		('a'..'z').each{|char| print char unless abc[char]}
+		('a'..'z').each{|char| return_value << char unless abc[char]}
+		return return_value
 	end
 
 end
 
 missingletters = MissingLetters.new()
 
-missingletters.getMissingLetters(ARGV[0])
+puts missingletters.getMissingLetters(ARGV[0])
